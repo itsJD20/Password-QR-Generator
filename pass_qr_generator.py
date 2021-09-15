d612a4a0ea96cfb677d8ae1220ab5f965db79928
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-09-16 00:45:58 +0600

    PassQR

---
+++ pass_qr_generator.py
@@ -0,0 +1,66 @@
+import random
+import string
+import qrcode
+
+    
+def rand(u):
+
+    return int(random.random() * u)
+
+def passgenV1(l=9):
+    character = (
+        list(string.ascii_lowercase) + 
+        list(string.ascii_uppercase) + 
+        [str(i) for i in range(10)] + 
+        ["*","$","#","@","&"]
+    )
+
+    password = ""
+    for i in range(l):
+
+        password += character[rand(len(character))]
+
+    return password
+
+def passgenV2(l = 8):
+
+    character = [list(string.ascii_lowercase),
+                list(string.ascii_uppercase),
+                [str(i) for i in range(10)],
+                ["*","$","#","@","&"]
+    ]
+    
+
+    password = ""
+
+    for i in range(l):
+        r1 = rand(4) #2
+        r2 = rand(len(character[r1]))
+        password += character[r1][r2] #2 5
+
+
+    return password
+
+img = qrcode.make(passgenV2())
+img.save("PASSWORD.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
